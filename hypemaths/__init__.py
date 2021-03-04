c32d3e4735d139dd5dd9f03b1e9e5f5f56a25beb
Sunrit Jana <warriordefenderz@gmail.com>
2021-03-04 23:06:31 +0530

    Convert the version into a fixed version [STABLE].

--- hypemaths/__init__.py
+++ hypemaths/__init__.py
@@ -5,6 +5,6 @@ from hypemaths.models import (
 
 __author__ = "Sunrit Jana"
 __email__ = "warriordefenderz@gmail.com"
-__version__ = "0.1.0-alpha"
+__version__ = "0.1.0"
 __licence__ = "GPL-3.0 License"
 __copyright__ = "Copyright 2020 Sunrit Jana"
