98dbde49aadf98dd8039ceccbafb962a55be590c
Rohith <007rohitjj04@gmail.com>
2021-03-12 16:00:54 +0530

    Edited the doc strings
    Co-authored-by: Sunrit Jana <warriordefenderz@gmail.com>

--- hypemaths/models/matrix.py
+++ hypemaths/models/matrix.py
@@ -561,7 +561,7 @@ class Matrix:
         return cls(matrix_list)
 
     def flatten(self, cls: list) -> t.Union[list, tuple, "hm.Vector"]:
-        '''
+        """
         Return a flattened version of the matrix.
         All elements of the matrix are placed into a single row.
         Parameters
@@ -579,7 +579,7 @@ class Matrix:
         >>> m.flatten()
         list([[1, 2, 3, 4]])
 
-        '''
+        """
         if isinstance(cls, (list, tuple, "hm.Vector")):
             raise TypeError(
                 f"Matrix can only be flattend to list, tuple or Vector. Got {cls}")
