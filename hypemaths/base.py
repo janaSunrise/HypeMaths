6d1ec0dc99bce31b4183f4eaaa1443b8aebf8456
janaSunrise <warriordefenderz@gmail.com>
2021-04-16 12:50:13 +0530

    Fix an error.

--- hypemaths/base.py
+++ hypemaths/base.py
@@ -5,4 +5,4 @@ import typing as t
 class Integral(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def format(self, func: t.Callable, a: t.Union[int, float], b: t.Union[int, float]) -> t.Any:
-        raise NotADirectoryError
+        raise NotImplementedError
